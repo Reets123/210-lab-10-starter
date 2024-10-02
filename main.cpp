97a1202147d6e5bac7ae1c53690883250ef9a2cf
Reets123 <ibrahimalatig@gmail.com>
2024-10-02 04:04:58 +0000

    commit 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+// Lab 10: Fork This Repo
+// COMSC-210 - Ibrahim Alatig 
+
 #include <iostream>
 using namespace std;
 
 int main() {
 
     // add any code here, sort of a 'hello world plus' code block
+    cout << "Forked the Repository" << endl;
 
     return 0;
 }
